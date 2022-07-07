d72a80ffe3f05ceff2b9f56678e516f3c456620f
minhsang <dangsangb3@gmail.com>
2022-07-07 14:51:48 +0700

    giải phương trình bậc 1

---
+++ hello-world/src/bac_1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class bac_1 {
+    public static void main(String[] args) {
+        System.out.println("bộ phân giải phương trình tuyến");
+        System.out.println("Biểu thức sau 'a * x + b = c',vui lòng nhập hằng số:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("a: ");
+        double a = scanner.nextDouble();
+        System.out.print("b: ");
+        double b = scanner.nextDouble();
+        System.out.print("c: ");
+        double c = scanner.nextDouble();
+        if (a != 0) {
+            double answer = (c - b) /a;
+            System.out.printf("Equation pass with x = %f!\n", answer);
+        }else {
+            if (b == c) {
+                System.out.print("Giải pháp là tất cả x");
+            }else {
+                System.out.print("k có giải pháp");
+            }
+        }
+    }
+}
