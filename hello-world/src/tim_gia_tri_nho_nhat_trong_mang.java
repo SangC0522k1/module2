54ee0a01065c2de2f0fbf7f127d913852b32b916
minhsang <dangsangb3@gmail.com>
2022-07-11 15:14:43 +0700

    tìm phần tử nhỏ nhất có trong mảng sử dụng phương thức

---
+++ hello-world/src/tim_gia_tri_nho_nhat_trong_mang.java
@@ -0,0 +1,17 @@
+public class tim_gia_tri_nho_nhat_trong_mang {
+    public static void main(String[] args) {
+        int[] array = {5, 12, 7, 6, 3, 9, 10};
+        int index = minValue(array);
+        System.out.println("Phần tử nhỏ nhất trong mảng là: " + array[index]);
+    }
+
+    public static int minValue (int[] array) {
+       int index = 0;
+       for (int i = 1; i < array.length; i++) {
+           if (array[i] < array[index]) {
+               index = i;
+           }
+       }
+       return index;
+    }
+}
