c83b485cf4b4cb92de6e8c9578db4ca9382aef48
minhsang <dangsangb3@gmail.com>
2022-07-11 14:26:24 +0700

    tìm giá trị trong mảng

---
+++ hello-world/src/tim_gia_tri_trong_mang.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class tim_gia_tri_trong_mang {
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập tên của học sinh:");
+        String input_name = scanner.nextLine();
+        boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(input_name)) {
+                System.out.println("Vị trí của các sinh viên " + input_name + " is: " + i);
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            System.out.println("Không tìm thấy" + input_name + "trong danh sách.");
+        }
+    }
+}
