3fcf2b2bfc8fdc20d25c829ba6554f4525e3ebd5
minhsang <dangsangb3@gmail.com>
2022-07-14 17:18:19 +0700

    Cài đặt thuật toán sắp xếp chọn

---
+++ Sort1/src/Thuc_hanh2.java
@@ -0,0 +1,26 @@
+public class Thuc_hanh2 {
+        static double[] list = {2, 9, 4.5, 3.6, 4.7, -2.5};
+        public static void selectionSort(double[] list) {
+            for (int i = 0; i < list.length - 1; i++) {
+            double currentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+                }
+            }
+        }
+        public static void main(String[] args) {
+            selectionSort(list);
+            for (int i = 0; i < list.length; i++)
+                System.out.print(list[i] + " ");
+        }
+    }
+
