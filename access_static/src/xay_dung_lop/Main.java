fa17fb87e85e4b93bd17ebfe6c1775051dcfe81b
minhsang <dangsangb3@gmail.com>
2022-07-20 16:49:20 +0700

    [Bài tập] Xây dựng lớp chỉ ghi trong Java

---
+++ access_static/src/xay_dung_lop/Main.java
@@ -0,0 +1,10 @@
+package xay_dung_lop;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setClasses("nhh");
+        student.setName("nam");
+        System.out.println(student);
+    }
+}
---
+++ access_static/src/xay_dung_lop/Student.java
@@ -0,0 +1,33 @@
+package xay_dung_lop;
+
+public class Student {
+    private String name = "John";
+    private String classes = "CO2";
+    public Student() {
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
