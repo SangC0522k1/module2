55c4892aef13ced8817b0ddadc6455b512655511
minhsang <dangsangb3@gmail.com>
2022-07-25 11:49:25 +0700

    [Bài tập] Lớp Point2D và lớp Point3D

---
+++ ke_thua/src/bt_point/Main.java
@@ -0,0 +1,9 @@
+package bt_point;
+
+public class Main {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        point3D.setXYZ(3.4f, 4.5f, 5.5f);
+        System.out.println(point3D);
+    }
+}
