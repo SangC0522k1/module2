30b91c98eafc156d937914c0a6a8c1e114cdb808
minhsang <dangsangb3@gmail.com>
2022-08-08 13:48:11 +0700

    Đảo ngược Stack

---
+++ stack_queue/src/bt_dao_nguoc_Stack.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class bt_dao_nguoc_Stack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack<String> reverse = new Stack<String>();
+
+        System.out.println("Nhap vao cac so hoac chuoi");
+        String s = sc.nextLine();
+        for (int i = 0; i <s.length() ; i++) {
+            reverse.push(s.charAt(i) + "");
+        }
+        System.out.println("chuoi dao nguoc: ");
+        for (int i = 0; i <s.length() ; i++) {
+            System.out.print(reverse.pop());
+        }
+    }
+}
