97bd033ec009c5191365eba4aa4c0d5a8452581e
minhsang <dangsangb3@gmail.com>
2022-08-22 10:43:19 +0700

    [Thực hành] Factory Method

---
+++ th_factory_method/Animal.java
@@ -0,0 +1,5 @@
+package th_factory_method;
+
+public abstract class Animal {
+    public abstract String makeSound();
+}
---
+++ th_factory_method/Dog.java
@@ -0,0 +1,9 @@
+package th_factory_method;
+
+public class Dog extends Animal{
+
+    @Override
+    public String makeSound() {
+        return "Woof";
+    }
+}
